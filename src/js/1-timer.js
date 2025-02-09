8efd6e35f2b2b4a74b3fcdf029b37da559bdc6e6
yegor <yegorveretelnik@gmail.com>
2025-02-09 09:34:24 +0100

    commit message

---
+++ src/js/1-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const datetimePicker = document.getElementById("datetime-picker"); 
+const startButton = document.querySelector("[data-start]");
+let userSelectedDate
+
+startButton.disabled = true;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    
+
+    if (userSelectedDate < new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  }
+};
+
+flatpickr(datetimePicker, options);
+
+
+let timerId = null;
+
+startButton.addEventListener('click', () => {
+
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+
+  timerId = setInterval(() => {
+    const remainingTime = userSelectedDate - new Date();
+
+    if (remainingTime <= 0) {
+      clearInterval(timerId);
+      updateTimerDisplay(0, 0, 0, 0);
+      datetimePicker.disabled = false;  
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(remainingTime);
+    updateTimerDisplay(days, hours, minutes, seconds);
+  }, 1000);
+});
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  function updateTimerDisplay(days, hours, minutes, seconds) {
+    document.querySelector("[data-days]").textContent = addLeadingZero(days);
+    document.querySelector("[data-hours]").textContent = addLeadingZero(hours);
+    document.querySelector("[data-minutes]").textContent = addLeadingZero(minutes);
+    document.querySelector("[data-seconds]").textContent = addLeadingZero(seconds);
+  }
+
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
